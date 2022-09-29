d27113f108535f4f6bc1a352f109b5ab84d9f547
Stefan Wieczorek <stefan.wieczorek@mgt-commerce.com>
2022-09-29 16:39:54 +0200

    .

--- varnish/enable.php
+++ varnish/enable.php
@@ -19,20 +19,28 @@ try {
     if (false === file_exists($sqliteDatabaseFile)) {
         throw new \Exception(sprintf('SQLite Database "%s" does not exist.', $sqliteDatabaseFile));
     }
-    $varnishControllerName = strtolower($application);
-    $varnishControllerFile = sprintf('%s/%s/controller.php', rtrim($varnishControllerDirectory, '/'), $varnishControllerName);
-    if (false === file_exists($varnishControllerFile)) {
-        throw new \Exception(sprintf('Varnish controller file %s not found.', $varnishControllerFile));
-    }
     $pdo = new \PDO(sprintf('sqlite:%s', $sqliteDatabaseFile));
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $statement = $pdo->prepare('SELECT * FROM site WHERE domain_name=:domainName');
+    $statement->bindParam(':domainName', $domainName,PDO::PARAM_STR);
+    $statement->execute();
+    $site = $statement->fetch(PDO::FETCH_ASSOC);
+    if (false === isset($site['id'])) {
+        throw new \Exception(sprint('Site with domain name %s does not exist.', $domainName));
+    }
+    $siteId = $site['id'];
     $statement = $pdo->prepare('SELECT * FROM vhost_template WHERE name=:application');
     $statement->bindParam(':application', $application,PDO::PARAM_STR);
     $statement->execute();
     $vhostTemplate = $statement->fetch(PDO::FETCH_ASSOC);
     if (true === isset($vhostTemplate['varnish_cache_settings']) && false === empty($vhostTemplate['varnish_cache_settings'])) {
         $varnishCacheSettings = json_decode($vhostTemplate['varnish_cache_settings'], true);
-        if (true === isset($varnishCacheSettings['cacheLifetime'])) {
+        if (true === isset($varnishCacheSettings['controller']) && true === isset($varnishCacheSettings['cacheLifetime'])) {
+            $varnishControllerName = $varnishCacheSettings['controller'];
+            $varnishControllerFile = sprintf('%s/%s/controller.php', rtrim($varnishControllerDirectory, '/'), $varnishControllerName);
+            if (false === file_exists($varnishControllerFile)) {
+                throw new \Exception(sprintf('Varnish controller file %s not found.', $varnishControllerFile));
+            }
             $muha = 1;
         }
     }
